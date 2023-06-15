5e4ce6ef6de91a2774d55b20b564f103fda87019
wambuiwambui <tnjoki11@gmail.com>
2023-06-15 22:40:36 +0300

    change procfile 5

--- run.py
+++ run.py
@@ -8,6 +8,4 @@ if __name__ == '__main__':
     cli = FlaskGroup(app)
     cli()
 
-    # Get the port from the environment variable or use a default value
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(DEBUG=True)
